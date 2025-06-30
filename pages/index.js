da3fc2db39b0b48c8b79dec7d4afcf3c3d5c76f5
MerelyLogical <qieerbushe7@gmail.com>
2025-06-30 22:05:34 +0100

    chap 32

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm learning how to use next.js!</p>
+        <p>I'm learning how to use next.js's page router!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
